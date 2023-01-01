7af603518136c833e09ae94ab080614331ee4ee6
OleksaBohdan <bohdan.oleksa@gmail.com>
2023-01-01 16:47:45 +0100

    wip: creating routes

--- cmd/main.go
+++ cmd/main.go
@@ -1,11 +1,23 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	routes "inex/main/internal/transport/routes"
+	"log"
+	"net/http"
+	// routes "inex/main/routes"
+)
 
 func main() {
-
 	fmt.Println("Hello inex")
 
+	routes.HttpRoutes()
 	
 
+	fmt.Println("Server started")
+	
+	err := http.ListenAndServe(":8000", nil)
+	if err != nil {
+		log.Fatal(err)
+	}
 }
