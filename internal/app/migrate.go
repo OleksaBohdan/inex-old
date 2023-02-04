f54a8ebdbb104dee174aaa3d64d30d3d681321c7
OleksaBohdan <bohdan.oleksa@gmail.com>
2023-02-04 16:57:35 +0100

    feat: migrate command

--- internal/app/migrate.go
+++ internal/app/migrate.go
@@ -1,17 +1,41 @@
 package app
 
 import (
+	"database/sql"
 	"fmt"
+	"github.com/golang-migrate/migrate/v4"
+	"github.com/golang-migrate/migrate/v4/database/postgres"
+	_ "github.com/golang-migrate/migrate/v4/source/file"
 	"inex/main/configs"
 	"log"
 )
 
 func init() {
 	fmt.Println("run migrate")
+
 	pg, err := configs.NewPG()
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	fmt.Println(pg.URL)
+	db, err := sql.Open("postgres", pg.URL)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	driver, err := postgres.WithInstance(db, &postgres.Config{})
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	m, err := migrate.NewWithDatabaseInstance(
+		pg.Migrations, "postgres", driver)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	err = m.Up()
+	if err != nil {
+		log.Println("migrate:", err)
+	}
 }
