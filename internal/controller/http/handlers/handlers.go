7aa799a6c72d874568e7adb51e4da75ec2fc5e34
OleksaBohdan <bohdan.oleksa@gmail.com>
2023-03-04 16:03:49 +0100

    feat: income item usecase & updated handlers struct

--- internal/controller/http/handlers/handlers.go
+++ internal/controller/http/handlers/handlers.go
@@ -2,11 +2,17 @@ package handlers
 
 import (
 	"fmt"
+	"github.com/gofrs/uuid"
 	"github.com/labstack/echo/v4"
 	"inex/main/domain"
 	"inex/main/internal/repository"
 	"inex/main/internal/usecase"
 	"net/http"
+	"sync"
+)
+
+var (
+	lock = sync.Mutex{}
 )
 
 type Handler struct {
@@ -18,69 +24,105 @@ func NewHandler(repo repository.InexRepo) *Handler {
 }
 
 func (h Handler) CreateNote(c echo.Context) error {
+	lock.Lock()
+	defer lock.Unlock()
+
 	var note domain.Note
 
 	err := c.Bind(&note)
 	if err != nil {
-		return c.JSON(http.StatusBadRequest, map[string]string{"error": "Invalid data"})
+		return c.NoContent(http.StatusBadRequest)
 	}
 
 	n, err := usecase.CreateNote(note, h.Repo)
 	if err != nil {
 		fmt.Println(err)
-		return c.JSON(http.StatusInternalServerError, map[string]string{"error": "Internal server error"})
+		return c.NoContent(http.StatusInternalServerError)
 	}
 
 	return c.JSON(http.StatusCreated, n)
 }
 
 func (h Handler) CreateIncomeItem(c echo.Context) error {
+	lock.Lock()
+	defer lock.Unlock()
+
 	var incomeItem domain.IncomeItem
 
 	err := c.Bind(&incomeItem)
 	if err != nil {
-		return c.JSON(http.StatusBadRequest, map[string]string{"error": "Invalid data"})
+		return c.NoContent(http.StatusBadRequest)
 	}
 
 	item, err := usecase.CreateIncomeItem(incomeItem, h.Repo)
 	if err != nil {
 		fmt.Println(err)
-		return c.JSON(http.StatusInternalServerError, map[string]string{"error": "Internal server error"})
+		return c.NoContent(http.StatusInternalServerError)
 	}
 
 	return c.JSON(http.StatusCreated, item)
 }
 
 func (h Handler) UpdateIncomeItem(c echo.Context) error {
+	lock.Lock()
+	defer lock.Unlock()
+
 	var incomeItem domain.IncomeItem
 
 	err := c.Bind(&incomeItem)
 	if err != nil {
-		return c.JSON(http.StatusBadRequest, map[string]string{"error": "Invalid data"})
+		return c.NoContent(http.StatusBadRequest)
 	}
 
 	item, err := usecase.UpdateIncomeItem(incomeItem, h.Repo)
 	if err != nil {
 		fmt.Println(err)
-		return c.JSON(http.StatusInternalServerError, map[string]string{"error": "Internal server error"})
+		return c.NoContent(http.StatusInternalServerError)
 	}
 
-	return c.JSON(http.StatusCreated, item)
+	return c.JSON(http.StatusOK, item)
 }
 
 func (h Handler) ReadIncomeItems(c echo.Context) error {
+	lock.Lock()
+	defer lock.Unlock()
+
 	var user domain.User
 
-	err := c.Bind(&user)
+	id, err := uuid.FromString(c.Param("id"))
 	if err != nil {
-		return c.JSON(http.StatusBadRequest, map[string]string{"error": "Invalid data"})
+		return c.NoContent(http.StatusBadRequest)
 	}
+
+	user.ID = id
 
 	items, err := usecase.ReadIncomeItems(user, h.Repo)
 	if err != nil {
 		fmt.Println(err)
-		return c.JSON(http.StatusInternalServerError, map[string]string{"error": "Internal server error"})
+		return c.NoContent(http.StatusInternalServerError)
 	}
 
-	return c.JSON(http.StatusCreated, items)
+	return c.JSON(http.StatusOK, items)
+}
+
+func (h Handler) DeleteIncomeItem(c echo.Context) error {
+	lock.Lock()
+	defer lock.Unlock()
+
+	var incomeItem domain.IncomeItem
+
+	id, err := uuid.FromString(c.Param("id"))
+	if err != nil {
+		return c.NoContent(http.StatusBadRequest)
+	}
+
+	incomeItem.ID = id
+
+	err = usecase.DeleteIncomeItem(incomeItem, h.Repo)
+	if err != nil {
+		fmt.Println(err)
+		return c.NoContent(http.StatusInternalServerError)
+	}
+
+	return c.NoContent(http.StatusNoContent)
 }
--- internal/controller/http/routes/routes.go
+++ internal/controller/http/routes/routes.go
@@ -2,6 +2,7 @@ package routes
 
 import (
 	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
 	"inex/main/internal/controller/http/handlers"
 	"inex/main/internal/repository"
 )
@@ -9,9 +10,14 @@ import (
 func RegisterRoutes(e *echo.Echo, repo *repository.InexRepo) {
 	handler := handlers.NewHandler(*repo)
 
+	e.Use(middleware.Logger())
+	e.Use(middleware.Recover())
+
 	e.POST("/api/v1/note", handler.CreateNote)
 
 	e.POST("/api/v1/income-item", handler.CreateIncomeItem)
-	e.GET("/api/v1/income-item", handler.ReadIncomeItems)
+	e.GET("/api/v1/income-item/:id", handler.ReadIncomeItems)
 	e.PUT("/api/v1/income-item", handler.UpdateIncomeItem)
+	e.DELETE("/api/v1/income-item/:id", handler.DeleteIncomeItem)
+
 }
